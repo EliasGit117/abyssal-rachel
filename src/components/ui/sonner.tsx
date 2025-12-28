117a87071c5faa8d197650f0bf6363938682cb55
EliasGit117 <95446990+EliasGit117@users.noreply.github.com>
2025-12-28 22:58:29 +0200

    Refactor `useTheme` import path in `sonner.tsx` for consistency and modularity.

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { useTheme } from "next-themes"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 import { IconCircleCheck, IconInfoCircle, IconAlertTriangle, IconAlertOctagon, IconLoader } from "@tabler/icons-react"
+import { useTheme } from '@/components/theme';
 
 const Toaster = ({ ...props }: ToasterProps) => {
   const { theme = "system" } = useTheme()
