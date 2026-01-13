7e895f3b0e9db3005177fa2dfb5a0fcd09fce419
EliasGit117 <95446990+EliasGit117@users.noreply.github.com>
2026-01-13 18:52:10 +0200

    Small fix for adaptive-button.tsx

--- src/components/ui/adaptive-button.tsx
+++ src/components/ui/adaptive-button.tsx
@@ -3,6 +3,7 @@ import { Button } from '@/components/ui/button.tsx';
 import { Icon } from '@tabler/icons-react';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip.tsx';
 import { cn } from '@/lib/utils.ts';
+import { VariantProps } from 'class-variance-authority';
 
 
 interface IAdaptiveButtonProps extends Pick<ComponentProps<typeof Button>, 'variant' | 'size' | 'onClick'> {
@@ -16,10 +17,17 @@ export const AdaptiveButton: FC<IAdaptiveButtonProps> = (props) => {
   const { icon: Icon, className, text, tooltipDelay = 500, size, variant } = props;
   return (
     <Tooltip delayDuration={tooltipDelay}>
-      <TooltipTrigger className={cn('aspect-square sm:aspect-auto w-fit', className)} asChild>
+      <TooltipTrigger
+        className={cn(
+          'aspect-square sm:aspect-auto w-fit',
+          widthBySize[size ?? 'default'],
+          className
+        )}
+        asChild
+      >
         <Button size={size} variant={variant}>
           {Icon && <Icon/>}
-          <span className='sr-only sm:not-sr-only'>{text}</span>
+          <span className="sr-only sm:not-sr-only">{text}</span>
         </Button>
       </TooltipTrigger>
 
@@ -28,4 +36,16 @@ export const AdaptiveButton: FC<IAdaptiveButtonProps> = (props) => {
       </TooltipContent>
     </Tooltip>
   );
+};
+
+const widthBySize: Record<NonNullable<VariantProps<typeof Button>['size']>, string> = {
+  default: 'w-9',
+  xs: 'w-6',
+  sm: 'w-8',
+  lg: 'w-10',
+  icon: 'w-9',
+  'icon-xs': 'w-6',
+  'icon-sm': 'w-8',
+  'icon-lg': 'w-10',
+  dense: 'w-fit'
 };
